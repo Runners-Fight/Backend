ea83cf7189b579735394a83bb117837712978ce6
west_east <west.east1832@gmail.com>
2025-06-25 14:50:53 +0900

    [#4] test: 테스트 커버리지 테스트 코드 작성

--- src/main/java/run/backend/domain/crew/controller/TestController.java
+++ src/main/java/run/backend/domain/crew/controller/TestController.java
@@ -3,6 +3,7 @@ package run.backend.domain.crew.controller;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TestController {
 
+    @GetMapping("/hello")
     public String testCode(){
+        System.out.println("Hello World!!");
         return "test";
     }
 }
