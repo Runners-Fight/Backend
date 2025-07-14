736033234f4cc904f18624576e44cb0495a22512
choiseoji <choiseoji123@naver.com>
2025-07-14 13:37:07 +0900

    [#14] feat: 유저 정보 저장, 조회 컨트롤러 추가

---
+++ src/main/java/run/backend/domain/member/controller/MemberController.java
@@ -0,0 +1,37 @@
+package run.backend.domain.member.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import run.backend.domain.member.dto.request.MemberInfoRequest;
+import run.backend.domain.member.dto.response.MemberInfoResponse;
+import run.backend.domain.member.entity.Member;
+import run.backend.domain.member.service.MemberServiceImpl;
+import run.backend.global.annotation.member.Login;
+import run.backend.global.common.response.CommonResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
+@Tag(name = "Members", description = "Member 관련 API")
+public class MemberController {
+
+    private final MemberServiceImpl memberService;
+
+    @Operation(summary = "유저 정보 저장", description = "온보딩에서 유저 정보를 저장하는 API 입니다.")
+    @PostMapping("/onboarding")
+    public CommonResponse<Void> saveMemberInfo(@Login Member member, MemberInfoRequest request) {
+
+        memberService.saveMember(member, request);
+        return new CommonResponse<>("유저의 기본 정보 저장 완료");
+    }
+
+    @Operation(summary = "유저 정보 조회", description = "마이페이지 상단 유저 정보를 조회하는 API 입니다.")
+    @GetMapping
+    public CommonResponse<MemberInfoResponse> getMemberInfo(@Login Member member) {
+
+        MemberInfoResponse response = memberService.getMemberInfo(member);
+        return new CommonResponse<>("유저 정보 조회 성공", response);
+    }
+}
