581a62d86d4f8298210a47cd25b908757fe393ff
choiseoji <choiseoji123@naver.com>
2025-07-14 13:37:44 +0900

    [#14] feat: 가입한 크루가 없는 경우 발생할 에러 추가

---
+++ src/main/java/run/backend/domain/member/exception/MemberErrorCode.java
@@ -0,0 +1,15 @@
+package run.backend.domain.member.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import run.backend.global.exception.ErrorCode;
+
+@Getter
+@AllArgsConstructor
+public enum MemberErrorCode implements ErrorCode {
+
+    MEMBER_NOT_JOINED_CREW(5001, "가입한 크루가 없습니다.");
+
+    private final int errorCode;
+    private final String errorMessage;
+}
