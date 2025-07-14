e03683263b31deac356265dd3504fc20b43e0d39
choiseoji <choiseoji123@naver.com>
2025-07-14 23:29:41 +0900

    [#14] feat: 인터페이스에 회원 정보 수정 추가

--- src/main/java/run/backend/domain/member/service/MemberService.java
+++ src/main/java/run/backend/domain/member/service/MemberService.java
@@ -1,11 +1,15 @@
 package run.backend.domain.member.service;
 
+import org.springframework.web.multipart.MultipartFile;
+import run.backend.domain.member.dto.request.MemberInfoRequest;
 import run.backend.domain.member.dto.response.MemberInfoResponse;
 import run.backend.domain.member.entity.Member;
 
 public interface MemberService {
 
     MemberInfoResponse getMemberInfo(Member member);
+
+    void updateMemberInfo(Member member, String imageStatus, MultipartFile image, MemberInfoRequest data);
 
 //    void updateMember();
 
