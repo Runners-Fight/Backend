fe2f24f2930240f2982b4bf2d1debe0507ac11fd
choiseoji <choiseoji123@naver.com>
2025-07-14 13:38:02 +0900

    [#14] feat: 가입한 크루 반환하는 레포지토리 코드 추가

--- src/main/java/run/backend/domain/member/repository/MemberRepository.java
+++ src/main/java/run/backend/domain/member/repository/MemberRepository.java
@@ -2,8 +2,18 @@ package run.backend.domain.member.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import run.backend.domain.crew.entity.Crew;
 import run.backend.domain.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByOauthId(String oauthId);
+
+    @Query("""
+    SELECT jc.crew
+    FROM JoinCrew jc
+    WHERE jc.member.id = :memberId
+    """)
+    Optional<Crew> findCrewByMemberId(@Param("memberId") Long memberId);
 }
