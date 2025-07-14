70c3579bd82e6bf3869e093371db20d6c2282ed4
choiseoji <choiseoji123@naver.com>
2025-07-14 13:36:23 +0900

    [#14] feat: login 한 유저 반환하는 어노테이션 추가

---
+++ src/main/java/run/backend/global/annotation/member/LoginArgumentResolver.java
@@ -0,0 +1,35 @@
+package run.backend.global.annotation.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import run.backend.domain.member.entity.Member;
+import run.backend.global.security.CustomUserDetails;
+
+@Component
+@RequiredArgsConstructor
+public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        boolean isMemberType = parameter.getParameterType().equals(Member.class);
+        return hasLoginAnnotation && isMemberType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
+        return principal.getMember();
+    }
+}
