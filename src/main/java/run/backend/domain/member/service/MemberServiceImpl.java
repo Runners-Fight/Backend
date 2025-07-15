4ab085ac65b917174eb1e56c04930f5c16ea3601
choiseoji <choiseoji123@naver.com>
2025-07-15 13:59:32 +0900

    [#14] feat: 멤버 정보 수정 서비스 코드 추가

--- src/main/java/run/backend/domain/member/service/MemberServiceImpl.java
+++ src/main/java/run/backend/domain/member/service/MemberServiceImpl.java
@@ -3,8 +3,11 @@ package run.backend.domain.member.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import run.backend.domain.crew.entity.Crew;
 import run.backend.domain.crew.enums.JoinStatus;
+import run.backend.domain.file.service.FileService;
+import run.backend.domain.member.dto.request.MemberInfoRequest;
 import run.backend.domain.member.dto.response.MemberInfoResponse;
 import run.backend.domain.member.entity.Member;
 import run.backend.domain.member.repository.MemberRepository;
@@ -15,6 +18,7 @@ import run.backend.domain.member.repository.MemberRepository;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final FileService fileService;
 
     @Override
     public MemberInfoResponse getMemberInfo(Member member) {
@@ -22,7 +26,30 @@ public class MemberServiceImpl implements MemberService {
         String crewName = memberRepository.findCrewByMemberIdAndStatus(member.getId(), JoinStatus.APPROVED)
                 .map(Crew::getName)
                 .orElse("N/A");
-
         return new MemberInfoResponse(member.getProfileImage(), member.getNickname(), crewName);
+    }
+
+    @Override
+    @Transactional
+    public void updateMemberInfo(Member member, String imageStatus, MultipartFile image, MemberInfoRequest data) {
+
+        switch (imageStatus) {
+
+            case "updated" :
+                String newImageName = fileService.saveProfileImage(image);
+                member.updateImage(newImageName);
+                break ;
+            case "removed" :
+                break ;
+        }
+
+        if (data.gender() != null)
+            member.updateGender(data.gender());
+        if (data.age() != null)
+            member.updateAge(data.age());
+        if (data.nickname() != null)
+            member.updateNickname(data.nickname());
+
+        memberRepository.save(member);
     }
 }
