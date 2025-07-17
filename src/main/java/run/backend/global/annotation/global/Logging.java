20a4690962548bf7257e754b8a39cc5e049ed99f
west_east <west.east1832@gmail.com>
2025-07-17 16:18:43 +0900

    [#19] feat: 서비스 계층 로그 어노테이션 파라미터 추가

--- src/main/java/run/backend/global/annotation/global/Logging.java
+++ src/main/java/run/backend/global/annotation/global/Logging.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ServiceLog {
+public @interface Logging {
     String description() default "";
 }
--- src/main/java/run/backend/global/annotation/global/ServiceLoggingAspect.java
+++ src/main/java/run/backend/global/annotation/global/ServiceLoggingAspect.java
@@ -7,25 +7,29 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
+import java.util.Arrays;
 
 @Aspect
 @Component
 @Slf4j
 public class ServiceLoggingAspect {
 
-    @Around("@annotation(run.backend.global.annotation.global.ServiceLog)")
+    @Around("@annotation(run.backend.global.annotation.global.Logging)")
     public Object logServiceMethodExecution(ProceedingJoinPoint joinPoint) throws Throwable {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         String methodName = signature.getMethod().getName();
         String className = joinPoint.getTarget().getClass().getSimpleName();
 
-        ServiceLog serviceLog = signature.getMethod().getAnnotation(ServiceLog.class);
+        Logging serviceLog = signature.getMethod().getAnnotation(Logging.class);
         String description = serviceLog.description();
 
-        log.info("--- [ServiceLog] {} - {}() 시작 ---", className, methodName);
-        if (!description.isEmpty()) {
-            log.info("--- [ServiceLog] 설명: {} ---", description);
+        Object[] args = joinPoint.getArgs();
+        String parameterString = "";
+        if (args != null && args.length > 0) {
+            parameterString = " (파라미터: " + Arrays.toString(args) + ")";
         }
+
+        log.info("--- [Logging] {} - {}(){}{} 시작 ---", className, methodName, parameterString, (description.isEmpty() ? "" : " - " + description));
 
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
@@ -35,7 +39,7 @@ public class ServiceLoggingAspect {
             result = joinPoint.proceed();
         } finally {
             stopWatch.stop();
-            log.info("--- [ServiceLog] {} - {}() 종료 (소요 시간: {} ms) ---", className, methodName, stopWatch.getTotalTimeMillis());
+            log.info("--- [Logging] {} - {}() 종료 (소요 시간: {} ms) ---", className, methodName, stopWatch.getTotalTimeMillis());
         }
         return result;
     }
