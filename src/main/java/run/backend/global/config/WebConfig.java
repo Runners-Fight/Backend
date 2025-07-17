e71d0338976a3621026c1cabe5991fae4f3bd745
west_east <west.east1832@gmail.com>
2025-07-17 19:12:02 +0900

    [#19] feat: 멤버가 속한 크루 어노테이션

--- src/main/java/run/backend/global/config/WebConfig.java
+++ src/main/java/run/backend/global/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import run.backend.global.annotation.member.LoginArgumentResolver;
+import run.backend.global.annotation.member.MemberCrewArgumentResolver;
 
 import java.util.List;
 
@@ -12,14 +13,18 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginArgumentResolver loginArgumentResolver;
+    private final MemberCrewArgumentResolver memberCrewArgumentResolver;
 
     @Autowired
-    public WebConfig(LoginArgumentResolver loginArgumentResolver) {
+    public WebConfig(LoginArgumentResolver loginArgumentResolver, 
+                     MemberCrewArgumentResolver memberCrewArgumentResolver) {
         this.loginArgumentResolver = loginArgumentResolver;
+        this.memberCrewArgumentResolver = memberCrewArgumentResolver;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginArgumentResolver);
+        resolvers.add(memberCrewArgumentResolver);
     }
 }
