bdb32aa1b086df1a0b661f81b48fed8fdc471be5
choiseoji <choiseoji123@naver.com>
2025-07-24 23:30:57 +0900

    [#26] feat: 가입된 크루 있는지 확인하는 코드 추가

--- src/main/java/run/backend/domain/member/controller/MemberController.java
+++ src/main/java/run/backend/domain/member/controller/MemberController.java
@@ -6,9 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import run.backend.domain.member.dto.request.MemberInfoRequest;
+import run.backend.domain.member.dto.response.MemberCrewStatusResponse;
 import run.backend.domain.member.dto.response.MemberInfoResponse;
 import run.backend.domain.member.entity.Member;
-import run.backend.domain.member.service.MemberServiceImpl;
+import run.backend.domain.member.service.MemberService;
 import run.backend.global.annotation.member.Login;
 import run.backend.global.common.response.CommonResponse;
 
@@ -18,7 +19,7 @@ import run.backend.global.common.response.CommonResponse;
 @Tag(name = "Members", description = "Member 관련 API")
 public class MemberController {
 
-    private final MemberServiceImpl memberService;
+    private final MemberService memberService;
 
     @Operation(summary = "유저 정보 조회", description = "마이페이지 상단 유저 정보를 조회하는 API 입니다.")
     @GetMapping
@@ -38,5 +39,13 @@ public class MemberController {
 
         memberService.updateMemberInfo(member, imageStatus, image, data);
         return new CommonResponse<>("유저 정보 수정 성공");
+    }
+
+    @GetMapping("/me/crews/exists")
+    @Operation(summary = "가입된 크루가 있는지 확인", description = "유저가 가입된 크루가 있는지 확인하는 API 입니다.")
+    public CommonResponse<MemberCrewStatusResponse> getMembersCrewExists(@Login Member member) {
+
+        MemberCrewStatusResponse response = memberService.getMembersCrewExists(member);
+        return new CommonResponse<>("유저 크루 가입 여부 조회 완료", response);
     }
 }
--- src/main/java/run/backend/domain/member/service/MemberService.java
+++ src/main/java/run/backend/domain/member/service/MemberService.java
@@ -1,21 +1,72 @@
 package run.backend.domain.member.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import run.backend.domain.crew.entity.Crew;
+import run.backend.domain.crew.entity.JoinCrew;
+import run.backend.domain.crew.enums.JoinStatus;
+import run.backend.domain.crew.repository.JoinCrewRepository;
+import run.backend.domain.file.service.FileService;
 import run.backend.domain.member.dto.request.MemberInfoRequest;
+import run.backend.domain.member.dto.response.MemberCrewStatusResponse;
 import run.backend.domain.member.dto.response.MemberInfoResponse;
 import run.backend.domain.member.entity.Member;
+import run.backend.domain.member.repository.MemberRepository;
 
-public interface MemberService {
+import java.util.Optional;
 
-    MemberInfoResponse getMemberInfo(Member member);
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
 
-    void updateMemberInfo(Member member, String imageStatus, MultipartFile image, MemberInfoRequest data);
+    private final FileService fileService;
+    private final MemberRepository memberRepository;
+    private final JoinCrewRepository joinCrewRepository;
 
-//    void updateMember();
+    public MemberInfoResponse getMemberInfo(Member member) {
 
-//    void deleteMember(Member member);
-//
-//    void leaveCrew(Member member, Long crewId);
-//
-//    void joinCrew(String crewCode);
+        String crewName = memberRepository.findCrewByMemberIdAndStatus(member.getId(), JoinStatus.APPROVED)
+                .map(Crew::getName)
+                .orElse("N/A");
+        return new MemberInfoResponse(member.getProfileImage(), member.getNickname(), crewName);
+    }
+
+    @Transactional
+    public void updateMemberInfo(Member member, String imageStatus, MultipartFile image, MemberInfoRequest data) {
+
+        switch (imageStatus) {
+
+            case "updated" :
+                fileService.deleteImage(member.getProfileImage());   // 기존 이미지 지우기
+                String newImageName = fileService.saveProfileImage(image);   // 새로운 이미지 저장
+                member.updateImage(newImageName);
+                break ;
+            case "removed" :
+                fileService.deleteImage(member.getProfileImage());
+                member.updateImage("default-profile-image.png");
+                break ;
+        }
+
+        if (data.gender() != null)
+            member.updateGender(data.gender());
+        if (data.age() != null)
+            member.updateAge(data.age());
+        if (data.nickname() != null)
+            member.updateNickname(data.nickname());
+
+        memberRepository.save(member);
+    }
+
+    public MemberCrewStatusResponse getMembersCrewExists(Member member) {
+
+        Optional<JoinCrew> joinCrew = joinCrewRepository.findByMember(member);
+
+        if (joinCrew.isEmpty()) {
+            return new MemberCrewStatusResponse("NONE");
+        }
+        return new MemberCrewStatusResponse(joinCrew.get().getJoinStatus().toString());
+    }
 }
