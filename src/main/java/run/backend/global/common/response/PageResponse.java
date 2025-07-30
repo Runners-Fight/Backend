738b4d81d9cd877a2af37612bc63ee39eacad2e6
choiseoji <choiseoji123@naver.com>
2025-07-30 13:45:49 +0900

    [#26] feat: PageResponse에 to메서드 추가

--- src/main/java/run/backend/global/common/response/PageResponse.java
+++ src/main/java/run/backend/global/common/response/PageResponse.java
@@ -1,5 +1,7 @@
 package run.backend.global.common.response;
 
+import org.springframework.data.domain.Page;
+
 import java.util.List;
 
 public record PageResponse<T>(
@@ -9,4 +11,16 @@ public record PageResponse<T>(
         long totalElements,
         boolean isLast,
         List<T> content
-) {}
+) {
+
+    public static <T> PageResponse<T> toPageResponse(Page<?> page, List<T> content) {
+        return new PageResponse<>(
+                page.getNumber(),
+                content.size(),
+                page.getTotalPages(),
+                page.getTotalElements(),
+                page.isLast(),
+                content
+        );
+    }
+}
