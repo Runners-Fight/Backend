f5e93a1d78cc66f290564599371e500891fc4c2e
choiseoji <choiseoji123@naver.com>
2025-08-02 00:19:50 +0900

    [#28] feat: CrewMemberResponse로 매핑하는 CrewMapper 코드 추가

--- src/main/java/run/backend/domain/crew/mapper/CrewMapper.java
+++ src/main/java/run/backend/domain/crew/mapper/CrewMapper.java
@@ -3,13 +3,12 @@ package run.backend.domain.crew.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
-import run.backend.domain.crew.dto.response.CrewBaseInfoResponse;
-import run.backend.domain.crew.dto.response.CrewProfileResponse;
-import run.backend.domain.crew.dto.response.CrewRankingResponse;
-import run.backend.domain.crew.dto.response.CrewRankingStatusResponse;
+import run.backend.domain.crew.dto.response.*;
 import run.backend.domain.crew.entity.Crew;
 import run.backend.domain.member.entity.Member;
+import run.backend.domain.member.enums.Role;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Mapper(
@@ -25,6 +24,26 @@ public interface CrewMapper {
     CrewRankingResponse toCrewRankingResponse(Crew crew);
 
     List<CrewRankingResponse> toCrewRankingResponseList(List<Crew> crews);
+
+    @Mapping(source = "profileImage", target = "image")
+    CrewMemberProfileResponse toCrewMemberProfile(Member member);
+
+    default CrewMemberResponse toCrewMemberResponse(List<Member> crewMembers) {
+        List<CrewMemberProfileResponse> managers = new ArrayList<>();
+        List<CrewMemberProfileResponse> members = new ArrayList<>();
+
+        for (Member member : crewMembers) {
+            CrewMemberProfileResponse profile = toCrewMemberProfile(member);
+
+            if (member.getRole() == Role.MEMBER) {
+                members.add(profile);
+            } else {
+                managers.add(profile);
+            }
+        }
+
+        return new CrewMemberResponse(managers, members);
+    }
 
     default Crew toEntity(String imageName, String name, String description) {
         return Crew.builder()
