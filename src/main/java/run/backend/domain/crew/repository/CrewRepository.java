ac397d334e3684a78e7d370cea3f8f7aaa029410
choiseoji <choiseoji123@naver.com>
2025-08-02 09:42:37 +0900

    [#28] feat: 검색 repository 코드 추가

--- src/main/java/run/backend/domain/crew/repository/CrewRepository.java
+++ src/main/java/run/backend/domain/crew/repository/CrewRepository.java
@@ -3,6 +3,8 @@ package run.backend.domain.crew.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import run.backend.domain.crew.dto.query.CrewProfileDto;
 import run.backend.domain.crew.entity.Crew;
 
 import java.util.Optional;
@@ -13,4 +15,15 @@ public interface CrewRepository extends JpaRepository<Crew, Long> {
 
 
     Page<Crew> findAllByOrderByMonthlyScoreTotalDesc(Pageable pageable);
+
+    @Query("""
+    SELECT new run.backend.domain.crew.dto.query.CrewProfileDto(
+        c.image,
+        c.name,
+        c.description
+    )
+    FROM Crew c
+    WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%'))
+    """)
+    Page<CrewProfileDto> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
