4932372962bc16ad25652dbc642d2443fb7311c1
choiseoji <choiseoji123@naver.com>
2025-10-22 16:58:01 +0900

    [#39] feat: 비관적 락 적용

--- src/main/java/run/backend/domain/running/repository/PixelRepository.java
+++ src/main/java/run/backend/domain/running/repository/PixelRepository.java
@@ -1,8 +1,18 @@
 package run.backend.domain.running.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import run.backend.domain.running.entity.Pixel;
 import run.backend.domain.running.entity.PixelId;
 
+import java.util.Optional;
+
 public interface PixelRepository extends JpaRepository<Pixel, PixelId> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Pixel p WHERE p.id = :id")
+    Optional<Pixel> findByIdWithLock(@Param("id") PixelId id);
 }
